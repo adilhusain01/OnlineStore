651f11246a4a42b65bf9f74b213d3a758a14442b
adilhusain01 <husainadil202@gmail.com>
2024-02-14 01:46:30 +0530

    Add error handling and not found middleware

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+require("express-async-errors"); 
 const app = express();
 const productRouter = require("./src/routes/productRoutes");
 const userRouter = require("./src/routes/userRoutes");
+const errorHandleMiddleWare = require("./src/middlewares/errorHandlers");
+const notFoundMiddleWare = require("./src/middlewares/notFound");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8000;
@@ -19,11 +22,23 @@ app.use(express.urlencoded({extended: false}));
 const dotenv = require("dotenv");
 dotenv.config();
 
+app.use(errorHandleMiddleWare);
+app.use(notFoundMiddleWare);
+
 mongoose.connect(`${process.env.MONGO_CONNECTION}`).then(() => console.log("Connectd to mongoDB")).catch((error) => console.log(error));
 
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 
-app.listen(PORT, () => {
-    console.log(`Server running on Port ${PORT}`);
-})
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server running on Port ${PORT}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
