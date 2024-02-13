395be9c1b6429a1c16b20685347f74b106111a08
adilhusain01 <husainadil202@gmail.com>
2024-02-13 16:50:32 +0530

    Update app.js to include CORS and user routes

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
-const router = require("./src/routes/productRoutes");
+const productRouter = require("./src/routes/productRoutes");
+const userRouter = require("./src/routes/userRoutes");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 8000;
 
+const cors = require("cors");
+app.use(cors({
+    origin:"http://localhost:8000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}))
+
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
-mongoose
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.use('/api/products', router);
+mongoose.connect(`${process.env.MONGO_CONNECTION}`).then(() => console.log("Connectd to mongoDB")).catch((error) => console.log(error));
+
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`);
