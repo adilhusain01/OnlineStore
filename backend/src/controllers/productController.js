c1d404c356a029e6b5cbd94296a2fdc8445e1bfc
adilhusain01 <husainadil202@gmail.com>
2024-02-14 01:45:51 +0530

    Add advanced querying and pagination to productController.js

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -1,9 +1,77 @@
 const Product = require("../models/productModel");
 const User = require("../models/userModel");
 
+
+//All the features are from mongoDB
 const getProducts = async (req, res) => {
     try {
-        const products = await Product.find();
+
+        //extracting the needed queries and isolate the products
+        const { shipping, company, name, sort, fields, numericFilters } = req.query;
+        const queryObject = {};
+
+        if(shipping){
+            queryObject.shipping = shipping === "true" ? true : false;
+        }
+
+        if(company){
+            queryObject.company = company; 
+        }
+
+        if(name){
+            queryObject.name = {$regex:name, $options:'i'};
+        }
+
+        if(numericFilters){
+            const operatorMap = {
+                '>':'$gt',
+                '<':'$lt',
+                '=':'$eq',
+                '>=':'$gte',
+                '<=':'$lte' 
+            }
+
+            const regEx = /\b(<|>|=|>=|<=)\b/g //regular expression
+            let filters = numericFilters.replace(regEx, (match) => `-${operatorMap[match]}-`);
+
+            const options = ['price', 'stars'];
+            filters = filters.split(',').forEach((item) => {
+                const [field, operator, value] = item.split('-');
+                if(options.includes(field)){
+                    queryObject[field] = {[operator]: Number(value)}
+                }
+            })
+        }
+
+        //firstly finding the particular objects if any query
+        let result = Product.find(queryObject);
+
+        //then if sorting is provided
+        if(sort){
+            //sort on basis of all parameters like alphabetical order and then in the same alphabet the price etcectra
+            const sortList = sort.split(',').join(' ');
+            result = result.sort(sortList);
+        }else{
+            //and Default is sort on basis of stars
+            result = result.sort('stars');
+        }
+
+        //getting the only fields we want
+        if(fields){
+            const selectList = fields.split(',').join(' ');
+            result = result.select(selectList);
+        }
+
+
+        //Setting up the pagination facility
+        const limit = Number(req.query.limit) || 5;
+        const page = Number(req.query.page) || 1;
+
+        const skip = (page-1)*limit;
+
+        result = result.skip(skip).limit(limit);
+        
+        const products = await result; 
         res.status(200).json(products);
     } catch (error) {
         res.status(400).json({ message: error.message });
